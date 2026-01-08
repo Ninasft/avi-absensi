66e838bb05b9a1b7f6051fd10e733a7eb09698e2
Siti Fatimah Tuma'ninah <siti.fatimah05@ui.ac.id>
2026-01-08 10:16:56 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  console.log("import.meta:", import.meta);
+console.log("import.meta.env:", import.meta.env);
   return <Component {...pageProps} />
 }
+
+console.log("import.meta:", import.meta);
+console.log("import.meta.env:", import.meta.env);
